7329caa1b333e433dca7245b91173fa773e021bd
simpson9241 <simpson9241@gmail.com>
2020-10-13 14:29:01 +0900

    commit

--- install.sh
+++ install.sh
@@ -4,17 +4,21 @@ n=0
 
 while [ $n -ne 5 ]
 do
+  clear;
 	echo "1. Install Python3 2. Install pip3  3. Install Redis, RQ  4. Install Everything  5. Exit"
 	echo "Input: "
 	read n
 	if [ $n -eq 1 ]
 	then
 		sudo apt-get install python3;
-		echo "Install and Configuration Complete";
     python3 --version
+		echo "Press Enter to Continue";
+    read enter;
 	elif [ $n -eq 2 ]
 	then
 		sudo apt-get install python3-pip;
+		echo "Press Enter to Continue";
+    read enter;
 	elif [ $n -eq 3 ]
 	then
     sudo apt-get update;
@@ -22,6 +26,8 @@ do
     sudo apt-get install redis-server;
     redis-server --version;
     sudo pip3 install rq;
+		echo "Press Enter to Continue";
+    read enter;
 	elif [ $n -eq 4 ]
 	then
     sudo apt-get install python3;
@@ -32,9 +38,13 @@ do
     sudo pip3 install rq;
     python3 --version;
     redis-server --version;
+		echo "Press Enter to Continue";
+    read enter;
 	elif [ $n -gt 5 -o $n -lt 1 ]
 	then
 		echo "Invalid Input!"
+		echo "Press Enter to Continue";
+    read enter;
 	else
 		echo "Exiting Prgram"
 	fi
